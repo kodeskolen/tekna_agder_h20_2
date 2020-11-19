73faf520c81043d43c28b685dc28ed638f9fce0f
einaj <einaj@mail.uio.no>
2020-11-19 18:13:52 +0100

    lagt til kode fra live gjennomgang

---
+++ dag2/live_koding/intro_lister.py
@@ -0,0 +1,7 @@
+
+
+navn = ["Anne", "Else", "Randi", "Hilde"]
+
+print(navn[0], navn[1], navn[2])
+
+print(navn)
---
+++ dag2/live_koding/lister_legge_til.py
@@ -0,0 +1,35 @@
+
+
+mnd = ["januar", "februar", "mars", "april", "mai", "juni"]
+
+mnd.append("juli")
+
+print(mnd)
+
+mnd.append("september")
+
+print(mnd)
+
+mnd.insert(7, "august")
+
+print(mnd)
+
+mnd += ["oktober", "november", "desember"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
